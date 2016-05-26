de500e6512461dc04fb84ad6161f1c540d8f3e9c
zy4290 <john.zhangyan@gmail.com>
2016-05-26 22:02:04 +0800

    增加了调试方法

--- server/api/v1/lbs/place.py
+++ server/api/v1/lbs/place.py
@@ -4,13 +4,18 @@
 import __config__
 from urllib import request
 import json
+import sys
 
 
 def get(query, region):
     url = __config__.get_baidu_api_url(query, region)
-    print(url)
     response = request.urlopen(url)
     if response:
         result = response.read().decode()
         response.close()
     return json.loads(result)['results']
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        raise Exception(sys.argv[0] + ' query region')
+    print(get(sys.argv[1], sys.argv[2]))
