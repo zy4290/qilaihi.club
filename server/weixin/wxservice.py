9890afb5e8311bb0f0b64e90761de7efe869f6ae
zy4290 <john.zhangyan@gmail.com>
2016-05-27 14:07:06 +0800

    修改微信token验证参数解析

--- server/weixin/wxservice.py
+++ server/weixin/wxservice.py
@@ -7,9 +7,9 @@ from tornado import web
 class WeiXinMessageHandler(web.RequestHandler):
 
     def get(self):
-        signature = self.request.arguments['signature']
-        timestamp = self.request.arguments['timestamp']
-        nonce = self.request.arguments['nonce']
-        echostr = self.request.arguments['echostr']
+        signature = self.request.arguments['signature'][0]
+        timestamp = self.request.arguments['timestamp'][0]
+        nonce = self.request.arguments['nonce'][0]
+        echostr = self.request.arguments['echostr'][0]
 
         self.write(echostr)
