defbb1facd783372018487b711cf8ee1307fa31d
zy4290 <john.zhangyan@gmail.com>
2016-05-29 01:29:46 +0800

    补充数据结构

--- server/model/config.py
+++ server/model/config.py
@@ -5,6 +5,20 @@ from peewee import *
 
 from model import dbutil
 
+"""
+CREATE TABLE `config` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `accesstoken` varchar(100) DEFAULT NULL,
+  `expires` int(11) DEFAULT NULL,
+  `appsecret` varchar(100) DEFAULT NULL,
+  `appid` varchar(100) DEFAULT NULL,
+  `token` varchar(100) DEFAULT NULL,
+  `encodingaeskey` varchar(100) DEFAULT NULL,
+  `baiduak` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+"""
+
 db = dbutil.get_db()
 
 
