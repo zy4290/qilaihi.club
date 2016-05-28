86328e07dbbc2b2160b5deb7c643c69bd4c25395
zy4290 <john.zhangyan@gmail.com>
2016-05-28 18:35:57 +0800

    上传一个数据库连接参数模板，生产环境需要调整对应的参数

---
+++ server/model/__param__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3.5
+# coding: utf-8
+
+from peewee import MySQLDatabase
+
+db = MySQLDatabase
+ip = 'ip'
+port = 3306
+user = 'user'
+password = 'password'
+database = 'database'
+charset = 'utf8'
