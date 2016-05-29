70d35e2fae81b29a2b2efd717a5878eaac749f0f
zy4290 <john.zhangyan@gmail.com>
2016-05-29 20:34:07 +0800

    新增微信access_token获取url

--- server/weixin/config.py
+++ server/weixin/config.py
@@ -18,3 +18,9 @@ event_type_dict = {
     'unsubscribe': None,
     'SCAN': None,
 }
+
+access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}'
+
+success_response = 'success'
+
+error_response = ''
