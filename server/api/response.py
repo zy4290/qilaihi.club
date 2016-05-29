c6558b2144dd473525cc2b1a1349a646fbb2600a
zy4290 <john.zhangyan@gmail.com>
2016-05-29 10:10:30 +0800

    为异常接口返回error msg，status为-1，msg为error，result为{}

---
+++ server/api/response.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3.5
+# coding: utf-8
+
+from tornado.escape import json_encode, json_decode
+
+
+class Response:
+
+    def __init__(self, status=-1, msg='error', result={}):
+        self.status = status
+        self.msg = msg
+        self.result = result
+
+    def dict(self):
+        return {
+            'status': self.status,
+            'msg': self.msg,
+            'result': self.result
+        }
+
+    def json(self):
+        return json_encode(self.dict())
+
--- server/api/v1/httpapi.py
+++ server/api/v1/httpapi.py
@@ -1,15 +1,24 @@
 #! /usr/bin/env python3.5
 # coding: utf-8
 
+import logging
+
 from tornado import web, escape
 
+from api.response import Response
 from api.v1.lbs import place
 
 
 class PlaceServiceHandler(web.RequestHandler):
 
-    def post(self):
-        request = escape.json_decode(self.request.body.decode())
-        query = request['query']
-        region = request['region']
-        self.write(place.get(query, region))
+    async def post(self):
+        try:
+            request = escape.json_decode(self.request.body.decode())
+            query = request['query']
+            region = request['region']
+            logging.debug('query: {0} region: {1}'.format(query, region))
+            response = await place.get(query, region)
+            self.write(response)
+        except Exception as e:
+            logging.error(str(e))
+            self.write(Response().json())
