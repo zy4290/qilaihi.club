b46c62a2fffa54f203ae8949abe7610bf788954d
zy4290 <john.zhangyan@gmail.com>
2016-05-29 12:22:00 +0800

    刷了一下格式

--- server/model/dbutil.py
+++ server/model/dbutil.py
@@ -5,6 +5,8 @@ from model import __param__
 
 
 def get_db():
-    return __param__.db(host=__param__.ip, port=__param__.port, user=__param__.user,
-                        password=__param__.password, database=__param__.database,
-                        charset=__param__.charset)
+    return __param__.db(
+        host=__param__.ip, port=__param__.port, user=__param__.user,
+        password=__param__.password, database=__param__.database,
+        charset=__param__.charset
+    )
