e31bc60fbf703b2d2f6c74b011bc00e2d6cd8beb
zy4290 <john.zhangyan@gmail.com>
2016-05-30 23:09:46 +0800

    新增发送客服消息

--- server/weixin/wxutil.py
+++ server/weixin/wxutil.py
@@ -1,30 +1,29 @@
 #! /usr/bin/env python3.5
 # coding: utf-8
 
+import copy
 import logging
+from concurrent.futures import ThreadPoolExecutor
 
-from tornado.httpclient import HTTPClient
 from tornado.escape import json_decode
-from tornado import gen
+from tornado.httpclient import AsyncHTTPClient
 
 from model.config import Config
-from weixin.config import access_token_url
+from weixin.config import access_token_url, custom_msg_url
 
 
-@gen.coroutine
-def refresh_access_token():
-    config = Config().select().get()
+async def refresh_access_token():
+    config = await ThreadPoolExecutor(1).submit(Config().select().get())
     try:
-        http_client = HTTPClient()
+        http_client = AsyncHTTPClient()
         logging.debug(access_token_url.format(config.appid, config.appsecret))
-        response = http_client.fetch(
+        response = await http_client.fetch(
             access_token_url.format(config.appid, config.appsecret))
         logging.debug(response.body.decode())
         result = json_decode(response.body.decode)
         config.accesstoken = result['access_token']
         config.expires = result['expires_in']
-        config.save()
-        return result['access_token']
+        await ThreadPoolExecutor(1).submit(config.save())
     except Exception as e:
         raise e
 
@@ -41,3 +40,24 @@ async def get_access_token():
 async def send_template_msg():
     # TODO 发送模板消息待完成
     pass
+
+
+_custom_text = {
+    'touser': None,
+    'msgtype': 'text',
+    'text': {
+        'content': None
+    }
+}
+
+
+async def send_custom_msg(msg, reply):
+    custom_text = copy.deepcopy(_custom_text)
+    custom_text['touser'] = msg.fromusername
+    custom_text['text']['content'] = reply
+
+    config = await ThreadPoolExecutor(1).submit(Config().select().get())
+    url = custom_msg_url.format(config.accesstoken)
+
+    http_client = AsyncHTTPClient()
+    await http_client.fetch(url, **{'body': custom_text})
