49da1c4c2d12850aca2911d656445aceb4dfbe8a
zy4290 <john.zhangyan@gmail.com>
2016-05-30 23:04:53 +0800

    新增custom_msg_url

--- server/weixin/config.py
+++ server/weixin/config.py
@@ -3,6 +3,8 @@
 
 access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}'
 
+custom_msg_url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={0}'
+
 success_response = 'success'
 
 error_response = ''
