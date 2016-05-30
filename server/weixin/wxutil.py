a740eb090a9748ce5b8211910fb870a76fda0728
zy4290 <john.zhangyan@gmail.com>
2016-05-30 23:42:53 +0800

    增加了数据库的连接和关闭代码

--- server/weixin/wxutil.py
+++ server/weixin/wxutil.py
@@ -9,6 +9,7 @@ from tornado import gen
 from tornado.escape import json_decode
 from tornado.httpclient import AsyncHTTPClient
 
+from model import dbutil
 from model.config import Config
 from weixin.config import access_token_url, custom_msg_url
 
@@ -25,9 +26,13 @@ def refresh_access_token():
         result = json_decode(response.body.decode)
         config.accesstoken = result['access_token']
         config.expires = result['expires_in']
+        dbutil.get_db().connect()
         yield ThreadPoolExecutor(1).submit(config.save())
     except Exception as e:
         raise e
+    finally:
+        if not dbutil.get_db().is_closed():
+            dbutil.get_db().close()
 
 
 @gen.coroutine
@@ -60,8 +65,16 @@ def send_custom_msg(msg, reply):
     custom_text['touser'] = msg.fromusername
     custom_text['text']['content'] = reply
 
-    config = yield ThreadPoolExecutor(1).submit(Config().select().get)
-    url = custom_msg_url.format(config.accesstoken)
+    url = ''
+    try:
+        dbutil.get_db().connect()
+        config = yield ThreadPoolExecutor(1).submit(Config().select().get)
+        url = custom_msg_url.format(config.accesstoken)
+    except Exception as e:
+        logging.error(str(e))
+    finally:
+        if not dbutil.get_db().is_closed():
+            dbutil.get_db().close()
 
     http_client = AsyncHTTPClient()
     yield http_client.fetch(url, **{'body': custom_text})
