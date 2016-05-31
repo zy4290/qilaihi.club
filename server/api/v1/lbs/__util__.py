148135ea56ec1ef2a6a92474a660df68b8bbf8b4
zy4290 <john.zhangyan@gmail.com>
2016-05-31 19:01:10 +0800

    修改为使用DBUtil查询百度ak

--- server/api/v1/lbs/__util__.py
+++ server/api/v1/lbs/__util__.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3.5
 # coding: utf-8
 
-from tornado.escape import url_escape
-
 from model.config import Config
+from model.dbutil import DBUtil
 
 
 def _get_baidu_ak():
-    return Config.select().get().baiduak
+    config = yield DBUtil.do(Config.select().get)
+    return config.baiduak
 
 
 def get_baidu_api_url(query, region, output='json'):
