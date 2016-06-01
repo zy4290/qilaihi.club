72dfa024d6a0943579285b4e6c70a7ddd874d3a2
zy4290 <john.zhangyan@gmail.com>
2016-06-01 23:39:07 +0800

    增加了发布活动的hanlder

--- server/api/v1/eventservice.py
+++ server/api/v1/eventservice.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3.5
 # coding: utf-8
 
+import datetime
 import json
 import logging
 
@@ -20,7 +21,7 @@ class SearchEventByCodeHandler(web.RequestHandler):
         for event in events:
             result = event
             break
-        return json.dumps(result, ensure_ascii=False, indent=4)
+        return result
 
     @gen.coroutine
     def post(self):
@@ -28,12 +29,47 @@ class SearchEventByCodeHandler(web.RequestHandler):
             request = json.loads(self.request.body.decode())
             code = request['code']
             logging.debug('code: {0}'.format(code))
-            response = yield DBUtil.do(self.query, code)
-            self.write(Response(status=1, msg='ok', result=response))
+            result = yield DBUtil.do(self.query, code)
+            if result['status'] == -1:
+                self.write(Response(
+                    status=1, msg='sorry，亲，该活动已关闭',
+                    result={}
+                ).json())
+            else:
+                self.write(Response(
+                    status=1, msg='ok',
+                    result=json.dumps(result, ensure_ascii=False, indent=4)
+                ).json())
         except Exception as e:
             self.write(Response().json())
             logging.exception('GetEventHandler error: {0}'.format(str(e)))
 
 
 class CreateEventHandler(web.RequestHandler):
-    pass
+    @gen.coroutine
+    def post(self):
+        try:
+            request = json.loads(self.request.body.decode())
+            event = Event(
+                code=request.get('code', None),
+                status=0,
+                logoimgurl=request.get('logoimgurl', None),
+                title=request.get('title', None),
+                time=request.get('time', None),
+                aacost=request.get('aacost', None),
+                tag=request.get('tag', None),
+                agerange=request.get('agerange', None),
+                location=request.get('location', None),
+                latitude=request.get('latitude', None),
+                longtitude=request.get('longtitude', None),
+                createtime=datetime.datetime.now(),
+                organizaerid=request.get('organizaerid', None)
+            )
+            yield DBUtil.do(event.save)
+            self.write(Response(
+                status=1, msg='恭喜你，活动发布成功！',
+                result=None
+            ).json())
+        except Exception as e:
+            self.write(Response().json())
+            logging.exception('CreateEventHandler error: {0}'.format(str(e)))
