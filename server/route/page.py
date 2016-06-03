e059d03bf1068e81c818ae9e1674611b7ef3b7ea
zy4290 <john.zhangyan@gmail.com>
2016-06-03 16:54:46 +0800

    增加了状态码

--- server/route/page.py
+++ server/route/page.py
@@ -6,13 +6,14 @@ from tornado import web
 
 class DefaultHandler(web.RequestHandler):
     def get(self):
-        self.write('404 Page Not Found')
+        self.write_error(404)
 
     def write_error(self, status_code, **kwargs):
+        self.set_status(status_code)
         if status_code == 404:
-            self.write('404 Page Not Found')
+            self.write('404 - Page Not Found')
         elif status_code == 500:
-            self.write('500 Server Internal Error')
+            self.write('500 - Internal Server Error')
         else:
             self.write(status_code)
 
