c80e1c0f32cc8a222e684f1306d8531027c192d8
zy4290 <john.zhangyan@gmail.com>
2016-06-03 19:37:13 +0800

    实现了微信oauth2接口的若干步骤

--- server/weixin/wxutil.py
+++ server/weixin/wxutil.py
@@ -1,16 +1,17 @@
 #! /usr/bin/env python3.5
 # coding: utf-8
 
-import copy
-import logging
-import json
+import copy, logging, json, datetime
 
 from tornado import gen
 from tornado.httpclient import AsyncHTTPClient
 
+from playhouse.shortcuts import dict_to_model
+
 from model.dbutil import DBUtil
+from model.user import User
 from model.config import Config
-from weixin.config import access_token_url, custom_msg_url
+from weixin import config as wxconfig
 
 
 @gen.coroutine
@@ -19,9 +20,9 @@ def refresh_access_token():
     config = yield DBUtil.do(Config.select().get)
 
     http_client = AsyncHTTPClient()
-    logging.debug(access_token_url.format(config.appid, config.appsecret))
+    logging.debug(wxconfig.access_token_url.format(config.appid, config.appsecret))
     response = yield http_client.fetch(
-        access_token_url.format(config.appid, config.appsecret))
+        wxconfig.access_token_url.format(config.appid, config.appsecret))
     logging.info(response.body.decode())
     result = json.loads(response.body.decode())
     config.accesstoken = result['access_token']
@@ -46,24 +47,15 @@ def send_template_msg():
     pass
 
 
-_custom_text = {
-    'touser': None,
-    'msgtype': 'text',
-    'text': {
-        'content': None
-    }
-}
-
-
 @gen.coroutine
 def send_custom_msg(msg, reply):
-    custom_text = copy.deepcopy(_custom_text)
+    custom_text = copy.deepcopy(wxconfig.custom_text_template)
     custom_text['touser'] = msg.fromusername
     custom_text['text']['content'] = reply
     logging.debug(custom_text)
 
     config = yield DBUtil.do(Config.select().get)
-    url = custom_msg_url.format(config.accesstoken)
+    url = wxconfig.custom_msg_url.format(config.accesstoken)
     logging.debug(url)
 
     logging.debug(json.dumps(custom_text, ensure_ascii=False, indent=4))
@@ -71,3 +63,53 @@ def send_custom_msg(msg, reply):
     response = yield http_client.fetch(url, **{'method': 'POST',
                                                'body': json.dumps(custom_text, ensure_ascii=False)})
     logging.debug(response.body.decode())
+
+@gen.coroutine
+def get_oauth2_access_code(code):
+    if code:
+        return '{}'
+
+    config = DBUtil.do(Config.get)
+    appid = config.appid
+    secret = config.appsecret
+    oauth2_url = wxconfig.oauth2_access_token_url.format(appid, secret, code)
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(oauth2_url)
+    logging.debug(response.body.decode())
+    return response.body.decode()
+
+@gen.coroutine
+def refresh_oauth2_access_code(refresh_token):
+    if refresh_access_token:
+        return '{}'
+
+    config = DBUtil.do(Config.get)
+    appid = config.appid
+    oauth2_refresh_url = wxconfig.oauth2_access_token_refresh_url.format(appid, refresh_token)
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(oauth2_refresh_url)
+    logging.debug(response.body.decode())
+    return response.body.decode()
+
+@gen.coroutine
+def validate_oauth2_access_code(access_token, openid):
+    if refresh_access_token or openid:
+        return '{}'
+
+    validate_url = wxconfig.validate_oauth2_access_token_url.format(access_token, openid)
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(validate_url)
+    logging.debug(response.body.decode())
+    return response.body.decode()
+
+@gen.coroutine
+def pull_user_info(access_token, openid):
+    pull_user_info_url = wxconfig.pull_user_info_url.format(access_token, openid)
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(pull_user_info_url)
+    logging.debug(response.body.decode())
+    user_dict = json.loads(response.body.decode())
+    new_user = dict_to_model(User, user_dict, ignore_unknown=True)
+    new_user.createtime = datetime.datetime.now()
+    DBUtil.do(new_user.save)
+    return response.body.decode()
