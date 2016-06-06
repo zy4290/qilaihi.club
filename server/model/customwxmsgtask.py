de18a9b69c5ee229955456bb8a12f50a9b7f7b0d
zy4290 <john.zhangyan@gmail.com>
2016-06-06 17:53:07 +0800

    客服消息实体类，用于保存待发送客服消息

---
+++ server/model/customwxmsgtask.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3.5
+# coding: utf-8
+
+from peewee import *
+
+from model import basemodel
+
+
+class CustomWXmsgTask(basemodel):
+    createtime = DateTimeField(null=True)
+    data = TextField(null=True)
+    status = IntegerField(null=True)
+    templateid = CharField(null=True)
+    touser = CharField(null=True)
+    updatetime = DateTimeField(null=True)
+    url = CharField(null=True)
+
+    class Meta:
+        db_table = 'customwxmsgtask'
+        schema = 'qilaihi'
