4424c821bf0738891263f649b9b3364afbc3ea3b
zy4290 <john.zhangyan@gmail.com>
2016-06-06 17:58:54 +0800

    多媒体文件的同步任务与历史任务

---
+++ server/model/syncmediatask.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3.5
+# coding: utf-8
+
+from peewee import *
+
+from model import basemodel
+
+
+class SyncMediaTask(basemodel):
+    createtime = DateTimeField(null=True)
+    eventid = IntegerField(null=True)
+    localpath = CharField(null=True)
+    mediaid = CharField(null=True)
+    ossurl = CharField(null=True)
+    status = IntegerField(null=True)
+    updatetime = DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'syncmediatask'
+        schema = 'qilaihi'
