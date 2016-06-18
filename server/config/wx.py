3245f434b2c9904188f361023affaad295b99ff2
zy4290 <john.zhangyan@gmail.com>
2016-06-18 22:52:31 +0800

    新增临时二维码获取url配置项

--- server/config/wx.py
+++ server/config/wx.py
@@ -18,7 +18,8 @@ get_user_info_url = 'https://api.weixin.qq.com/cgi-bin/user/info?' + \
 custom_msg_url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token={0}'
 temp_resource_download_url = 'http://api.weixin.qq.com/cgi-bin/media/get?access_token={0}&media_id={1}'
 temp_resource_upload_url = 'http://api.weixin.qq.com/cgi-bin/media/upload?access_token={0}&type={1}'
-
+temp_qrcode_url = 'https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token={0}'
+temp_qrcode_ticket_url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket={0}'
 success_response = 'success'
 error_response = ''
 custom_text_template = {'touser': None, 'msgtype': 'text', 'text': {'content': None}}
