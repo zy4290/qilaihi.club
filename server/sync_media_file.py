8532eb21b402ce0014ac393dde7563ccf62ef086
zy4290 <john.zhangyan@gmail.com>
2016-06-18 23:15:48 +0800

    对于没有mediaids的活动记录，直接设置为已同步。

--- server/sync_media_file.py
+++ server/sync_media_file.py
@@ -26,33 +26,39 @@ def sync_media_file():
                 event = yield dbutil.do(Event.select().where(
                     (Event.syncfinish != 1) | (Event.syncfinish >> None)).order_by(+Event.createtime).get)
                 logging.info('开始处理活动[#{0}] 的media文件'.format(event.code))
-                all = json.loads(event.mediaids)
-                logging.debug('all ' + str(all))
-                if event.syncstatus:
-                    uploaded = json.loads(event.syncstatus)
+                if event.mediaids:
+                    all = json.loads(event.mediaids)
+                    logging.debug('all ' + str(all))
+                    if event.syncstatus:
+                        uploaded = json.loads(event.syncstatus)
+                    else:
+                        uploaded = []
+                    if event.imgurls:
+                        img_urls = json.loads(event.imgurls)
+                    else:
+                        img_urls = {}
+                    to_upload = list(set(all) - set(uploaded))
+                    logging.info('待处理media文件清单' + str(to_upload))
+                    results = yield [wxutil.process_temp_resource(media_id) for media_id in to_upload]
+                    for i in range(0, len(results)):
+                        if results[i] is not None:
+                            uploaded.append(to_upload[i])
+                            img_urls[to_upload[i]] = results[i]
+                    event.syncstatus = json.dumps(uploaded, ensure_ascii=False)
+                    event.imgurls = json.dumps(img_urls, ensure_ascii=False)
+                    logging.info('已完成media文件清单:' + str(uploaded))
+                    if set(all) == set(uploaded):
+                        logging.info('活动[#{0}] 所有media文件已完成'.format(event.code))
                 else:
-                    uploaded = []
-                if event.imgurls:
-                    img_urls = json.loads(event.imgurls)
-                else:
-                    img_urls = {}
-                to_upload = list(set(all) - set(uploaded))
-                logging.info('待处理media文件清单' + str(to_upload))
-                results = yield [wxutil.process_temp_resource(media_id) for media_id in to_upload]
-                for i in range(0, len(results)):
-                    if results[i] is not None:
-                        uploaded.append(to_upload[i])
-                        img_urls[to_upload[i]] = results[i]
-                event.syncstatus = json.dumps(uploaded, ensure_ascii=False)
-                event.imgurls = json.dumps(img_urls, ensure_ascii=False)
-                logging.info('已完成media文件清单:' + str(uploaded))
-                if set(all) == set(uploaded):
-                    logging.info('活动[#{0}] 所有media文件已完成'.format(event.code))
-                    event.syncfinish = 1
+                    logging.info('mediaids不存在，没有media文件需要同步')
+                event.syncfinish = 1
                 yield dbutil.do(event.save)
             except DoesNotExist:
-                logging.warning('无待同步media文件, sleep...')
+                logging.info('无待同步media文件, sleep...')
                 yield gen.sleep(60 * 10)
+            except Exception as e:
+                logging.exception('同步media文件发生异常：{0}, 退出...'.format(str(e)))
+                break
         else:
             yield gen.sleep(60 * 10)
 
