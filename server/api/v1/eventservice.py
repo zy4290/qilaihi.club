eeb1138c263d2aab7b2026d1426a9521ca202f21
zy4290 <john.zhangyan@gmail.com>
2016-06-20 18:29:13 +0800

    新增按番号全文检索活动的handler

--- server/api/v1/eventservice.py
+++ server/api/v1/eventservice.py
@@ -5,28 +5,19 @@ import datetime
 import json
 import logging
 
-import peewee
-from playhouse.shortcuts import dict_to_model, model_to_dict
+from playhouse.shortcuts import dict_to_model
 from tornado import gen
 from tornado import ioloop
+from xpinyin import Pinyin
 
 from api.postonlyhandler import PostOnlyHandler
 from api.response import Response
 from model import dbutil
 from model.event import Event
-from weixin import wxutil
+from service import event as event_service
 
 
 class GetEventHandler(PostOnlyHandler):  # 按番号查询活动
-
-    @staticmethod
-    def query(code):
-        try:
-            event = Event.get(Event.code == code)
-            result = model_to_dict(event)
-        except peewee.DoesNotExist:
-            result = {}
-        return result
 
     @gen.coroutine
     def post(self):
@@ -34,7 +25,7 @@ class GetEventHandler(PostOnlyHandler):  # 按番号查询活动
             request = json.loads(self.request.body.decode())
             code = request['code']
             logging.debug('code: {0}'.format(code))
-            result = yield dbutil.do(self.query, code)
+            result = yield event_service.query_by_code(code)
             if result['status'] == -1:
                 self.write(Response(
                     status=1, msg='sorry，亲，该活动已关闭',
@@ -50,14 +41,25 @@ class GetEventHandler(PostOnlyHandler):  # 按番号查询活动
             logging.exception('GetEventHandler error: {0}'.format(str(e)))
 
 
-class PublishEventHandler(PostOnlyHandler):  # 创建活动
+class QueryEventHandler(PostOnlyHandler):  # 按番号查询活动
 
-    @staticmethod
     @gen.coroutine
-    def get_qrcode(event):
-        event.qrcodeurl = yield wxutil.get_temp_qrcode_url(event.get_id())
-        event.qrcodecreatetime = datetime.datetime.now()
-        yield dbutil.do(event.save)
+    def post(self):
+        try:
+            request = json.loads(self.request.body.decode())
+            code = request['code']
+            logging.debug('code: {0}'.format(code))
+            result = yield event_service.query_fulltext_code(code)
+            self.write(Response(
+                status=1, msg='ok',
+                result=result
+            ).json())
+        except Exception as e:
+            self.write(Response(msg='sorry，亲，查询活动失败').json())
+            logging.exception('GetEventHandler error: {0}'.format(str(e)))
+
+
+class PublishEventHandler(PostOnlyHandler):  # 创建活动
 
     @gen.coroutine
     def post(self):
@@ -66,12 +68,19 @@ class PublishEventHandler(PostOnlyHandler):  # 创建活动
             logging.debug(data)
             event = dict_to_model(Event, data)
             event.createtime = datetime.datetime.now()
+            pinyin = Pinyin()
+            event.codepinyin = pinyin.get_pinyin(event.code, splitter=' ')
+            initials = []
+            for letter in pinyin.get_initials(event.code, splitter=' ').lower().split(' '):
+                if letter.isalpha():
+                    initials.append(letter * 4)
+            event.codepinyininitials = ' '.join(initials)
             yield dbutil.do(event.save)
             self.write(Response(
                 status=1, msg='恭喜你，活动发布成功！',
                 result={}
             ).json())
-            ioloop.IOLoop.current().spawn_callback(self.get_qrcode, event)
+            ioloop.IOLoop.current().spawn_callback(event_service.create_qrcode, event)
         except Exception as e:
             self.write(Response(msg='sorry，亲，活动发布失败').json())
             logging.exception('CreateEventHandler error: {0}'.format(str(e)))
